10cc8caa538d3886bbb24de7a8cf41cb1b321a55
Emanoel Viana <emanoelvianads@gmail.com>
2025-03-08 18:16:24 -0300

    Ajustes de layout e introdução de animações de abertura

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const sectionMain = document.querySelector('#sc-main')
+console.log(sectionMain)
+
+window.addEventListener('scroll', function() {
+    const scrollPosition = this.window.scrollY
+    const opacity = 1 - (scrollPosition / 200)
+
+    if(sectionMain.style.opacity >= 0) {
+        sectionMain.style.opacity = opacity
+    } else {
+        sectionMain.style.opacity = 0
+    }
+})
